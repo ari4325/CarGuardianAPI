20ac61941c9644f973a93fca5ceb1b984eb934b9
Arishmit Ghosh <ari4325@live.com>
2021-07-10 12:09:03 +0530

    Update load_messages.js

--- routes/load_messages.js
+++ routes/load_messages.js
@@ -1,1 +1,23 @@
-import 
+import Message from "../schema/MessageSchema.js";
+
+var arr = [];
+
+const sendmessage = async(req, res) => {
+   var cursor = Message.find({recipient_id: req.query.id});
+
+  cursor.each(function(err, item) {
+      // If the item is null then the cursor is exhausted/empty and closed
+      if(item!=null){
+        arr.push(item);
+      }
+      // otherwise, do something with the item
+  });
+  
+  res.json(
+    {
+      error: false,
+      message: "Fetch successful",
+      msg: arr
+    });
+  
+}
