108a1e592773c8a830907b1bd87e3e45dc0bcb77
Arishmit Ghosh <ari4325@live.com>
2021-07-10 12:21:34 +0530

    Update loadMessages.js

--- routes/loadMessages.js
+++ routes/loadMessages.js
@@ -2,6 +2,7 @@ import Message from "../schema/MessageSchema.js";
 
 const loadMessage = async(req, res) => {
    var arr = Message.find({recipient_id: req.query.id});
+   console.log(arr);
   
   res.json(
     {
